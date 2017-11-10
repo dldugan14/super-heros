d7b494d3c5ddaf317a396b9070271e10d1d069bb
Dillon <dugan_dillon@yahoo.com>
2017-11-10 15:25:51 -0700

    getting up to date

---
+++ src/components/App.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+
+import HeroesList from "./HeroesList";
+import HeroForm from "./HeroForm";
+import Dashboard from "./Dashboard";
+import "./app.css";
+import { getHeroes } from "../superheroes.service";
+
+class App extends Component {
+  state = {
+    heroes: []
+  };
+  //life kyle hooks. do stuff on initialize
+  componentWillMount() {
+    getHeroes()
+      .then(res => res.json())
+      .then(payload => {
+        this.setState({
+          heroes: payload.heroes
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  //update our super in the array, in place
+  handleSubmit = (event, hero) => {
+    // const hero = this.state.selectedHero;
+    const heroIndex = this.state.heroes.map(o => o.id).indexOf(hero.id);
+    this.setState({
+      selectedHero: {
+        id: undefined,
+        superhero: ""
+      },
+      heroes: [
+        ...this.state.heroes.slice(0, heroIndex),
+        {
+          ...hero,
+          superhero: hero.superhero
+        },
+        ...this.state.heroes.slice(heroIndex + 1, this.state.heroes.length)
+      ]
+    });
+  };
+
+  getHeroById = id => {
+    const hero = this.state.heroes.find(hero => hero.id === id);
+    return hero;
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <h1>Super Hero Town</h1>
+          <nav>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink exact to="/heroes">
+              Heroes
+            </NavLink>
+          </nav>
+          <hr />
+
+          <Route
+            exact
+            path="/"
+            render={props => <Dashboard heroes={this.state.heroes} />}
+          />
+          <Route
+            exact
+            path="/heroes"
+            render={props => <HeroesList heroes={this.state.heroes} />}
+          />
+          <Route
+            exact
+            path={"/heroes/details/:heroId"}
+            render={props => (
+              <HeroForm
+                {...props}
+                handleSubmit={this.handleSubmit}
+                getHeroById={this.getHeroById}
+              />
+            )}
+          />
+        </div>
+      </Router>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  heroes: state.heroes
+});
+
+export default connect(mapStateToProps)(App);
---
+++ src/components/HeroesList.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import HeroesListItem from "./HeroesListItem";
+import "./HeroesList.css";
+
+class HeroesList extends Component {
+  state = {
+    title: "Super Heroes",
+    selectedHero: {
+      id: undefined,
+      superhero: ""
+    }
+  };
+
+  handleSelectedHero = hero => {
+    // this.state.selectedHero = hero;
+    this.setState({
+      selectedHero: hero
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-sm-12">
+              <ul className="heroes">
+                <HeroesListItem
+                  heroes={this.props.heroes}
+                  onSelectedHero={this.handleSelectedHero}
+                />
+              </ul>
+            </div>
+            {this.state.selectedHero.id && (
+              <div className="col-md-6 col-sm-12">
+                <h2 style={{ textAlign: "center" }}>
+                  {this.state.selectedHero.superhero}
+                </h2>
+                <Link to={`/heroes/details/${this.state.selectedHero.id}`}>
+                  <button className="btn btn-info">Details</button>
+                </Link>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HeroesList;
